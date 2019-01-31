d7d679c7c6fda47e877d312427e9f079767af7ae
Noctum <evstigneevnm@gmail.com>
2019-01-31 22:54:53 +0400

    test instantiation corrected

--- Linsolver_test/source/test_inst/class_file_impl.cu
+++ Linsolver_test/source/test_inst/class_file_impl.cu
@@ -1,13 +1,6 @@
 #include "class_file_impl.cuh"
 
-template<typename T>
-__global__ void add_vectors_kernel(int N, const T*& x, T*& y)
-{
-    unsigned int j = blockIdx.x*blockDim.x + threadIdx.x;
-    if(j>=N) return;
 
-    y[j]+=x[j];
-
-}
-
+template class class_file<float>;
+template class class_file<double>;
 
--- Linsolver_test/source/test_inst/class_file_impl.cuh
+++ Linsolver_test/source/test_inst/class_file_impl.cuh
@@ -3,6 +3,17 @@
 #include <cuda_runtime.h> 
 #include "class_file.h"
 
+template<typename T>
+__global__ void add_vectors_kernel(int N, const T*& x, T*& y)
+{
+
+    unsigned int j = blockIdx.x*blockDim.x + threadIdx.x;
+    if(j>=N) return;
+
+    y[j]+=x[j];
+
+}
+
 template<typename T, int BLOCK_SIZE>
 void class_file<T, BLOCK_SIZE>::add_vectors(const T*& x, T*& y)
 {
@@ -12,5 +23,3 @@ void class_file<T, BLOCK_SIZE>::add_vectors(const T*& x, T*& y)
 }
 
 
-template class class_file<float>;
-template class class_file<double>;
