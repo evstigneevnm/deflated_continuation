5cb7a3132ee88b4753515a88e1573e1ab6f9a426
evstigneevnm <evstigneevnm@gmail.com>
2024-10-15 14:07:12 +0300

    fixed bug

--- Linsolver_test/source/models/KF_3D/test_Kolmogorov_3D_Newton.cpp
+++ Linsolver_test/source/models/KF_3D/test_Kolmogorov_3D_Newton.cpp
@@ -186,6 +186,7 @@ int main(int argc, char const *argv[])
     {
         KF_3D->exact_solution(Rey, x0);
         printf("exact solution norm = %le, div = %le\n", vec_ops->norm(x0), KF_3D->div_norm(x0));
+        vec_ops->assign_scalar(0, x1);
         KF_3D->sinus_perturbation_y(pert_magnitude, pert_ny, pert_nz, x1);
         vec_ops->add_mul(1.0, x1, x0);
         KF_3D->write_solution_abs("x_0.pos", x0);
